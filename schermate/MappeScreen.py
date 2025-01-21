2ac9ee02dc17d7afbf58ea80dfb6fbc3e296b9e9
casian <mitocarucasian@protonmail.com>
2025-01-21 11:41:53 +0100

    Rimozione env condiviso, creazione menu con bottoni

---
+++ schermate/MappeScreen.py
@@ -0,0 +1,17 @@
+import arcade
+
+class MappeScreen(arcade.View):
+
+    def on_update(self, delta_time):
+        print("mappe da fare")
+
+    def on_show(self):
+        arcade.set_background_color(arcade.color.DARK_GRAY)
+
+    def on_draw(self):
+        self.clear()
+        arcade.draw_text("Gestione mappe", self.window.width / 2, self.window.height / 2, arcade.color.WHITE, font_size=30, anchor_x="center")
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.ESCAPE:
+            self.window.show_view(MenuScreen())
---
+++ schermate/ShopScreen.py
@@ -0,0 +1,17 @@
+import arcade
+
+class ShopScreen(arcade.View):
+
+    def on_update(self, delta_time):
+        print("shop da fare")
+
+    def on_show(self):
+        arcade.set_background_color(arcade.color.GOLD)
+
+    def on_draw(self):
+        self.clear()
+        arcade.draw_text("Benvenuto nello shop!", self.window.width / 2, self.window.height / 2, arcade.color.WHITE, font_size=30, anchor_x="center")
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.ESCAPE:
+            self.window.show_view(MenuScreen())
