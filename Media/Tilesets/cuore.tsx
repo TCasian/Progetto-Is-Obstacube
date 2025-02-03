5c71217053c31db8c1022df4776367f89a2c276c
SimoneB04 <buonocoreformale@gmail.com>
2025-02-03 19:45:47 +0100

    Delete Media/Tilesets directory

--- Media/Tilesets/cuore.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.1" name="cuore" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="cuori.png" width="60" height="60"/>
-</tileset>
