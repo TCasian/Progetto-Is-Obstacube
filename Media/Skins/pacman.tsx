ad6554dca166c1c96962eff044e42591b6e2b408
SimoneB04 <buonocoreformale@gmail.com>
2025-02-05 19:16:32 +0100

    Add files via upload

---
+++ Media/Skins/pacman.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="pacman" tilewidth="50" tileheight="50" tilecount="2" columns="1">
+ <image source="pacman.png" width="50" height="100"/>
+</tileset>
